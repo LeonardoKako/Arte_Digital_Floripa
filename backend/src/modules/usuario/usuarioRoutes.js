6848ec9eade94dc30002ac16246d4a877a30bcb6
caio schultz <caio.mazziero.schultz@gmail.com>
2026-05-25 19:57:11 -0300

    feat: coloca middleware de autenticação nas rotas

--- backend/src/modules/usuario/usuarioRoutes.js
+++ backend/src/modules/usuario/usuarioRoutes.js
@@ -1,16 +1,17 @@
 import express, { Router } from 'express'
 import usuarioController from './usuarioController.js';
+import { authMiddleware } from '../../middleware/authMiddleware.js';
 
 const router = Router();
 
-router.get("/listar", usuarioController.listarUsuarios);
+router.get("/listar", authMiddleware, usuarioController.listarUsuarios);
 
-router.get("/listar/:id", usuarioController.listarUsuarioPorId);
+router.get("/listar/:id", authMiddleware, usuarioController.listarUsuarioPorId);
 
-router.post("/criar", usuarioController.criarUsuario);
+router.post("/criar", authMiddleware, usuarioController.criarUsuario);
 
-router.patch("/atualizar/:id", usuarioController.atualizarUsuario);
+router.patch("/atualizar/:id", authMiddleware, usuarioController.atualizarUsuario);
 
-router.delete("/deletar/:id", usuarioController.deletarUsuario);
+router.delete("/deletar/:id", authMiddleware, usuarioController.deletarUsuario);
 
 export default router;

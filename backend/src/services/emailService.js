3fd140c8bfae77a7699abf1130096862becdbb45
caio schultz <caio.mazziero.schultz@gmail.com>
2026-06-01 20:30:20 -0300

    chore: descomenta código do email service

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,30 +1,29 @@
-// import nodemailer from 'nodemailer';
-// import 'dotenv/config'
+import nodemailer from 'nodemailer';
+import 'dotenv/config'
 
-// const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS
-//     }
-// });
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
 
-// async function enviarEmailBoasVindas(email, senhaTemporaria) {
-//     await transporter.sendMail({
-//         from: process.env.EMAIL_USER,
-//         to: email,
-//         subject: 'Bem-vindo ao Arte Digital Floripa',
-//         html: `
-//             <h1>Olá, seja Bem-vindo!</h2>
-//             <p>Sua conta foi criada na plataforma Arte Digital Floripa.</p>
-//             <p><b>Email:</b> ${email}</p>
-//             <p><b>Senha temporária:</b> ${senhaTemporaria}</p>
-//             <p>Acesse o portal e altere sua senha após o primeiro login.</p>
-//             <a href="${process.env.FRONTEND_URL}/login">Acessar o portal</a>
-//         `
-//     });
-// }
+async function enviarEmailBoasVindas(email, token) {
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: 'Bem-vindo ao Arte Digital Floripa',
+        html: `
+            <h1>Olá, seja bem-vindo!</h1>
+            <p>Sua conta foi criada na plataforma Arte Digital Floripa.</p>
+            <p><b>Email:</b> ${email}</p>
+            <p>Para acessar o portal, clique no botão abaixo e crie sua senha:</p>
+            <a href="${process.env.FRONTEND_URL}/completar-cadastro?token=${token}">Criar minha senha</a>
+        `
+    });
+}
 
-// export default {
-//     enviarEmailBoasVindas
-// }
+export default {
+    enviarEmailBoasVindas
+}

5cbbc152588e3c3a69b77d07f295c55cf6e19d4d
caio schultz <caio.mazziero.schultz@gmail.com>
2026-06-19 10:42:07 -0300

    fix: aumenta tamanho aceito no json

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,9 @@ import { errorMiddleware } from './middleware/errorMiddleware.js';
 const app = express();
 const port = 3000;
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+    limit: '50mb'
+}));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/usuarios", usuarioRoutes);
